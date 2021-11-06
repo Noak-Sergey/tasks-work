22117426c88b54c624893e657d9e11a0301599df
Noak-Sergey <noakstuff@gmail.com>
2021-11-06 12:35:14 +0300

    ninth commit - complete h8

--- src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
+++ src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
@@ -18,24 +18,24 @@ beforeEach(() => {
 test('sort name up', () => {
     const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
 
-    expect(newState[0]).toBe({_id: 1, name: 'Александр', age: 66})
-    expect(newState[1]).toBe({_id: 3, name: 'Виктор', age: 44})
+    expect(newState[0]).toStrictEqual({_id: 1, name: 'Александр', age: 66})
+    expect(newState[1]).toStrictEqual({_id: 3, name: 'Виктор', age: 44})
     console.log(newState)
     // expect(...).toBe(...)
 })
 test('sort name down', () => {
     const newState = homeWorkReducer(initialState, {type: 'sortD', payload: 'down'})
 
-    expect(newState[5]).toBe({_id: 1, name: 'Кот', age: 3})
+    expect(newState[5]).toStrictEqual({_id: 1, name: 'Александр', age: 66})
     console.log(newState)
 })
 test('check age 18', () => {
     const newState = homeWorkReducer(initialState, {type: 'check', payload: 'up18'})
 
-    expect(newState[0]).toBe({_id: 1, name: 'Александр', age: 66})
-    expect(newState[1]).toBe({_id: 3, name: 'Виктор', age: 44})
-    expect(newState[2]).toBe({_id: 4, name: 'Дмитрий', age: 40})
-    expect(newState[3]).toBe({_id: 5, name: 'Ирина', age: 55})
+    expect(newState[0]).toStrictEqual({_id: 1, name: 'Александр', age: 66})
+    expect(newState[1]).toStrictEqual({_id: 3, name: 'Виктор', age: 44})
+    expect(newState[2]).toStrictEqual({_id: 4, name: 'Дмитрий', age: 40})
+    expect(newState[3]).toStrictEqual({_id: 5, name: 'Ирина', age: 55})
 
     console.log(newState)
 })
